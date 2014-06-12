7c0aa4467211fc67f293d9800a76696e536558ac
Daniel+ <admin@Mac-mini.local>
2014-06-12 20:39:01 +0200

    more indentation

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
   <?php
-  error_reporting(E_ALL);
-  header("Expires: on, 01 Jan 1970 00:00:00 GMT");
-  header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-  header("Cache-Control: no-store, no-cache, must-revalidate");
+    error_reporting(E_ALL);
+    header("Expires: on, 01 Jan 1970 00:00:00 GMT");
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+    header("Cache-Control: no-store, no-cache, must-revalidate");
     header("Cache-Control: post-check=0, pre-check=0", false);
     header("Pragma: no-cache");
     require "thumbs.php";
