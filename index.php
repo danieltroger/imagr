38c9473ac429659e2e09f8d0210586bada33639b
Daniel <daniel@natur-kultur.eu>
2014-12-26 11:42:12 +0100

    Fix the text encoding

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     error_reporting(E_ALL);
+    header("Content-type: text/html; charset=utf-8")
     header("Expires: on, 01 Jan 1970 00:00:00 GMT");
     header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
     header("Cache-Control: no-store, no-cache, must-revalidate");
