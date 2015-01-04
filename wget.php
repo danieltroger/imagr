464e7c4e9f17526afd6947f83c32d2fb9cbb018c
Daniel on PI <daniel@natur-kultur.eu>
2015-01-04 17:08:11 +0100

    Generate download files for wget
    Install wget and issue the following command to download all images:
    
    wget -ci <(wget -qO - http://yoursite/imagr/wget.php)

--- wget.php
+++ wget.php
@@ -3,10 +3,12 @@ require "imgs.php";
 require "thumbs.php";
 header("Content-type: text/plain");
 $a = imgs();
-$b = explode("/",getextension($_SERVER['SCRIPT_NAME'],true)[1]);
+$b = explode("/",getextension($_SERVER['PHP_SELF'],true)[1]);
 unset($b[sizeof($b)-1]);
 $b = implode("/",$b);
-$c = "http://" . $_SERVER['SERVER_ADDR'] . ":" . $_SERVER['SERVER_PORT'] . $b . "/download.php/";
+$p = explode("/",$_SERVER['SERVER_PROTOCOL']);
+$p = strtolower($p[0]);
+$c =  $p . "://" . $_SERVER['SERVER_NAME'] . ":" . $_SERVER['SERVER_PORT'] . $b . "/download.php/";
 foreach($a as $d)
 {
   echo $c . urlencode($d) . PHP_EOL;
