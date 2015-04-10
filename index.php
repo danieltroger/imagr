c5e92721edff4fed5238df1d5a1f76c6c634f0e8
Daniel on umini <daniel@s.natur-kultur.eu>
2015-04-10 13:53:57 +0200

    Hide please upload overlay on drop

--- index.php
+++ index.php
@@ -337,6 +337,7 @@
                     var src = files[i];
                     if(src.type.match(/image.*/))
                     {
+                      if(imgs.length != 0 && typeof empty != "undefined" && empty != undefined && empty.style.display != "none") empty.style.display = "none";
                       read(src);
                     }
                     else
@@ -454,7 +455,6 @@
                   {
                     imgs.push(ret.file);
                     addimg(ret.file);
-                    if(imgs.length != 0 && typeof empty != "undefined" && empty != undefined && empty.style.display != "none") empty.style.display = "none";
                     console.log(ret.orig_file+" was successfully uploaded");
                   }
                   else
